5ba1202e2439044bba6f0ac74a6a3cb266f13436
Suttipong Srikok <fisca2012@gmail.com>
2014-01-24 15:38:00 +0700

    Install opencart v1.5.6.1.

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/store/admin/');
+define('HTTP_CATALOG', 'http://localhost/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\AppServ\www\store/admin/');
+define('DIR_SYSTEM', 'C:\AppServ\www\store/system/');
+define('DIR_DATABASE', 'C:\AppServ\www\store/system/database/');
+define('DIR_LANGUAGE', 'C:\AppServ\www\store/admin/language/');
+define('DIR_TEMPLATE', 'C:\AppServ\www\store/admin/view/template/');
+define('DIR_CONFIG', 'C:\AppServ\www\store/system/config/');
+define('DIR_IMAGE', 'C:\AppServ\www\store/image/');
+define('DIR_CACHE', 'C:\AppServ\www\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\AppServ\www\store/download/');
+define('DIR_LOGS', 'C:\AppServ\www\store/system/logs/');
+define('DIR_CATALOG', 'C:\AppServ\www\store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'store');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\AppServ\www\store/catalog/');
+define('DIR_SYSTEM', 'C:\AppServ\www\store/system/');
+define('DIR_DATABASE', 'C:\AppServ\www\store/system/database/');
+define('DIR_LANGUAGE', 'C:\AppServ\www\store/catalog/language/');
+define('DIR_TEMPLATE', 'C:\AppServ\www\store/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\AppServ\www\store/system/config/');
+define('DIR_IMAGE', 'C:\AppServ\www\store/image/');
+define('DIR_CACHE', 'C:\AppServ\www\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\AppServ\www\store/download/');
+define('DIR_LOGS', 'C:\AppServ\www\store/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'store');
+define('DB_PREFIX', 'oc_');
+?>
